ade8041893afe5bcd6de40b33a853d41a32dc04c
Nick Fitzgerald <fitzgen@gmail.com>
2016-09-30 19:57:55 -0700

    Add a bench for when the trace ring buffer is inside a Mutex<T>

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,6 +7,7 @@ mod benches {
 
     use self::hydra::traits::TraceSink;
     use self::hydra::simple_trace::{SimpleTrace, SimpleTraceBuffer};
+    use std::sync::Mutex;
 
     #[bench]
     fn trace_ring_buffer_small_capacity(b: &mut test::Bencher) {
@@ -19,6 +20,16 @@ mod benches {
     fn trace_ring_buffer_large_capacity(b: &mut test::Bencher) {
         let mut buffer = SimpleTraceBuffer::new(2 * 1024 * 1024);
         b.iter(|| buffer.trace(SimpleTrace::FooEvent));
+        test::black_box(buffer);
+    }
+
+    #[bench]
+    fn trace_ring_buffer_in_mutex(b: &mut test::Bencher) {
+        let buffer = Mutex::new(SimpleTraceBuffer::new(2 * 1024 * 1024));
+        b.iter(|| {
+            let mut buffer = buffer.lock().unwrap();
+            buffer.trace(SimpleTrace::FooEvent)
+        });
         test::black_box(buffer);
     }
 }
